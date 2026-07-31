7276e3bbcd072ab3bc1b2561e1437f3b548836cc
Victor <victtors.web@hotmail.com>
2026-07-31 12:49:22 +0200

    Condicionais em Java

--- Ola.java
+++ Ola.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Ola {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner (System.in);
-
-        System.out.println("Qual o seu nome?");
-        String nome = leitor.nextLine();
+        Scanner leitor = new Scanner(System.in);
 
         System.out.println("Quantos anos você tem?");
         int idade = leitor.nextInt();
 
-        System.out.println("Olá, " + nome + "! Você tem " + idade + " anos.");
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade e pode tirar a habilitação");
+        } else if (idade >= 16) {
+            System.out.println("A partir de 16 anos você pode fazer as aulas, mas não tirar a habilitação.");
+        } else {
+            System.out.println("Você é menor de idade. Portanto, não pode tirar habilitação agora.");
         }
     }
-    
+}
