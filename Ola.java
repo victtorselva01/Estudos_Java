82d30926f13e53a69d9ea5c5f09d680e5c77119b
Victor <victtors.web@hotmail.com>
2026-08-02 09:54:43 +0200

    primeira tabuada

--- Ola.java
+++ Ola.java
@@ -4,15 +4,11 @@ public class Ola {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Quantos anos você tem?");
-        int idade = leitor.nextInt();
+        System.out.println("Digite um número para ver a tabuada:");
+        int numero = leitor.nextInt();
 
-        if (idade >= 18) {
-            System.out.println("Você é maior de idade e pode tirar a habilitação");
-        } else if (idade >= 16) {
-            System.out.println("A partir de 16 anos você pode fazer as aulas, mas não tirar a habilitação.");
-        } else {
-            System.out.println("Você é menor de idade. Portanto, não pode tirar habilitação agora.");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
         }
     }
 }
